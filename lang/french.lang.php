9f88e3d500d5bdf79196a13345db8092ccac7204
Rbeuque74 <rbeuque74@gmail.com>
2011-06-10 01:32:52 +0200

    first work --> affichage ticket user + vue complète d'un ticket

---
+++ lang/french.lang.php
@@ -0,0 +1,34 @@
+<?php
+define("_SUPPORT","Support");
+define("_PASPROPRIOTICKET","Vous n'etes pas propriétaire du ticket");
+define("_YOUWROTE","Vous avez écrit le ");
+define("_WROTE"," a écrit le ");
+define("_FLOODCMAIL","Vous avez d�ja post� un email il y'a moins de " . $nuked['contact_flood'] . " minutes,<br />veuillez patienter avant de renvoyer un autre email...");
+
+define("_NOENTRANCE","D�sol� mais vous n'avez pas les droits pour acc�der � cette page");
+define("_ZONEADMIN","Cette zone est r�serv�e a l'Admin, D�sol�...");
+define("_NOEXIST","D�sol� cette page n'existe pas ou l'adresse que vous avez tap� est incorrecte");
+define("_ADMINCONTACT","Administration Contact");
+define("_HELP","Aides");
+define("_DELETEMESSAGEFROM","Vous �tes sur le point de supprimer le message de");
+define("_LISTMAIL","Liste des messages");
+define("_PREFS","Pr�f�rences");
+define("_TITLE","Titre");
+define("_NAME","Nom");
+define("_DATE","Date");
+define("_READMESS","Lire");
+define("_DEL","Supprimer");
+define("_BACK","Retour");
+define("_FROM","De");
+define("_THE","le");
+define("_NOMESSINDB","Aucun message dans la base de donn�es");
+define("_READTHISMESS","Lire ce message");
+define("_DELTHISMESS","Supprimer ce message");
+define("_MESSDELETE","Message supprim� avec succ�s");
+define("_PREFUPDATED","Pr�f�rences modifi�es avec succ�s.");
+define("_EMAILCONTACT","Email de reception");
+define("_FLOODCONTACT","Dur�e en minutes entre 2 messages (flood)");
+define("_NOTCON","Vous avez re�u un mail contact");
+define("_ACTIONDELCONTACT","a supprim� un mail contact re�u");
+define("_ACTIONPREFCONT","a modifi� les pr�f�rences du module contact");
+?>
