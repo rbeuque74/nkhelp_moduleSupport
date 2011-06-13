380e0d45a192ce8a77124345dcdf16b9d357b171
Rbeuque74 <rbeuque74@gmail.com>
2011-06-14 00:26:59 +0200

    config avec les 4 variables colors

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+// -------------------------------------------------------------------------//
+// Nuked-KlaN - PHP Portal                                                  //
+// http://www.nuked-klan.org                                                //
+// -------------------------------------------------------------------------//
+// This program is free software. you can redistribute it and/or modify     //
+// it under the terms of the GNU General Public License as published by     //
+// the Free Software Foundation; either version 2 of the License.           //
+// -------------------------------------------------------------------------//
+
+$color_top = $bgcolor3;
+$color_content1 = $bgcolor2;
+$color_content2 = $bgcolor1;
+$color_admin = "orange";
+
+?>
